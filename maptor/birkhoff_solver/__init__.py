c0c6805da53d360213c817da3b3c86a3d92dba57
ChenDavidTimothy <chendavidtimothy@gmail.com>
2025-06-22 21:13:36 +0200

    remove birkhoff

--- maptor/birkhoff_solver/__init__.py
+++
@@ -1,8 +0,0 @@
-from ..mtor_types import OptimalControlSolution
-from .core_birkhoff_solver import _solve_multiphase_birkhoff_collocation
-
-
-__all__ = [
-    "OptimalControlSolution",
-    "_solve_multiphase_birkhoff_collocation",
-]
