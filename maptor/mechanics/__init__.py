84fa723ac6c922387d2bd12d9592cb5c1bcf75ec
ChenDavidTimothy <chendavidtimothy@gmail.com>
2025-06-28 09:07:31 +0200

    dependencies

--- maptor/mechanics/__init__.py
+++ maptor/mechanics/__init__.py
@@ -1,17 +1,4 @@
-try:
-    from .symbolic import lagrangian_to_maptor_dynamics  # type: ignore[misc]
+from .symbolic import lagrangian_to_maptor_dynamics
 
-    __all__ = ["lagrangian_to_maptor_dynamics"]
-except ImportError:
 
-    def lagrangian_to_maptor_dynamics(*args, **kwargs):
-        """Convert SymPy LagrangesMethod to MAPTOR dynamics format.
-
-        Raises:
-            ImportError: SymPy is not installed.
-        """
-        raise ImportError(
-            "SymPy is required for mechanics utilities.\nInstall with: pip install sympy"
-        )
-
-    __all__ = ["lagrangian_to_maptor_dynamics"]
+__all__ = ["lagrangian_to_maptor_dynamics"]
