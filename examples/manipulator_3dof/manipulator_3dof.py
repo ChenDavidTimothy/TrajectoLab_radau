29d989abef0fc6bbbbae27f1facfb073560ca66a
ChenDavidTimothy <chendavidtimothy@gmail.com>
2025-06-28 17:25:32 +0200

    anima

--- examples/manipulator_3dof/manipulator_3dof.py
+++ examples/manipulator_3dof/manipulator_3dof.py
@@ -305,13 +305,14 @@ problem.minimize(energy)
 # Mesh Configuration and Parameterized Initial Guess
 # ============================================================================
 
-phase.mesh([6, 6, 6], [-1.0, -1 / 3, 1 / 3, 1.0])
+num_interval = 5
+degree = [5]
+final_mesh = degree * num_interval
+phase.mesh(final_mesh, np.linspace(-1.0, 1.0, num_interval + 1))
 
-# Generate parameterized initial guess based on calculated joint angles
 states_guess = []
 controls_guess = []
-
-for N in [6, 6, 6]:
+for N in final_mesh:
     tau = np.linspace(-1, 1, N + 1)
     t_norm = (tau + 1) / 2
 
